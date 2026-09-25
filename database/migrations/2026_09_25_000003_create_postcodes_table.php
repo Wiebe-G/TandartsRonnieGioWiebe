3b38f5fb0f13f969110ae8b2731cdf4cc217a00b
Wiebe G. <153119096+Wiebe-G@users.noreply.github.com>
2026-09-25 12:07:27 +0200

    Migrations van Gio
    Migrations om database te doen

---
+++ database/migrations/2026_09_25_000003_create_postcodes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('postcodes', function (Blueprint $table) {
+            $table->string('adress')->primary();
+            $table->string('street');
+            $table->string('residence');
+            $table->string('house_number', 10);
+            $table->string('city');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('postcodes');
+    }
+};
